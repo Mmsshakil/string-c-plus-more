eca3f3aefcfa9b48f5536d493e7ca4cf2c869355
Mmsshakil <softhandshakil@gmail.com>
2025-06-04 00:37:50 +0600

    sort string

---
+++ sort_string.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s << endl;
+
+    sort(s.begin(), s.end()); // sort(begin, end)
+    cout << s << endl;
+
+    return 0;
+}
