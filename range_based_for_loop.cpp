b8521992d08c8f25afbad05de18a863be3b266f9
Mmsshakil <softhandshakil@gmail.com>
2025-06-04 01:10:46 +0600

    print array  without index

---
+++ range_based_for_loop.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    // int arr[5] = {1, 2, 3, 4, 5};
+
+    cin >> s;
+    for (char c : s)
+    {
+        cout << c << endl;
+    }
+
+    // for (int c : arr)
+    // {
+    //     cout << c << endl;  //output - 1 2 3 4 5
+    // }
+
+    return 0;
+}
